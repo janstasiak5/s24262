136077d763053ba55c83666570d462ef203b64fb
janstasiak5 <91740647+janstasiak5@users.noreply.github.com>
2021-10-21 23:09:03 +0200

    Add files via upload

---
+++ s03-echo.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace::std;
+int main()
+{
+string lol;
+
+	cout << "prosze o slowa:  " <<endl;
+	while (cin >> lol)
+{
+        cout << lol;
+        cout << " ";
+	}
+
+
+	return 0;
+}
